daca2e56487bdb1a4fb7962e532e4140533e736d
solofrank07 <fgutierresbpacherres@gmail.com>
2026-04-22 01:41:17 -0500

    Se agrega el proyecto

---
+++ backend/locator-api/src/test/java/com/mype/locator_api/LocatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mype.locator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
